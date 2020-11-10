5e0c989d1a996d61285f680e186570c3e3d9095a
Raisa Isabel Saravia Peralta <raisa_99_11@hotmail.com>
2020-11-10 10:18:33 -0300

    Se agrego entry point

---
+++ SPractica01/PPractica01/Principal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "----DATOS PERSONALES----" << endl;
+	cout << "Nombre: Raisa Isabel" << endl;
+	cout << "Apellidos: Saravia Peralta" << endl;
+	cout << "Cu: 35-4555" << endl;
+	cout << "Ci: 7526667" << endl;
+	cout << "Fec. Nacimiento: 06/09/1999" << endl;
+	system("pause");
+	return 0;
+}
